2eb122965998431fd8fec9bd4c4fef4d9e6c7c56
Steven Johnson <sjohnson0684@gmail.com>
2016-11-19 17:50:32 -0500

    Note to note name method

--- src/Pitch.php
+++ src/Pitch.php
@@ -25,4 +25,23 @@ class Pitch
         $centsOff = 100 * ($note - 69);
         return self::$standardFrequency * pow(2, $centsOff / 1200);
     }
+
+    public static function noteToNoteName($note)
+    {
+        $note = round($note);
+        switch ($note % 12) {
+            case 0: return 'C';
+            case 1: return 'C#';
+            case 2: return 'D';
+            case 3: return 'Eb';
+            case 4: return 'E';
+            case 5: return 'F';
+            case 6: return 'F#';
+            case 7: return 'G';
+            case 8: return 'G#';
+            case 9: return 'A';
+            case 10: return 'Bb';
+            case 11: return 'B';
+        }
+    }
 }
--- tests/PitchTest.php
+++ tests/PitchTest.php
@@ -15,4 +15,13 @@ class PitchTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(880, Pitch::noteToFrequency(81));
         $this->assertEquals(220, Pitch::noteToFrequency(57));
     }
+
+    public function testNoteToNoteNameMethod()
+    {
+        $this->assertEquals('C', Pitch::noteToNoteName(0));
+        $this->assertEquals('C', Pitch::noteToNoteName(60));
+        $this->assertEquals('A', Pitch::noteToNoteName(69));
+        $this->assertEquals('A', Pitch::noteToNoteName(69.4));
+        $this->assertEquals('Bb', Pitch::noteToNoteName(69.5));
+    }
 }
